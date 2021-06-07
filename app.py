32fdc22db9c2bb198b6a4278d97a477ccd862bcd
Amber-Williams <amberswebdesign@gmail.com>
2021-06-07 17:33:05 +0100

    basic working flask web application

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # because folder name is templates you don't have to specify route
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
