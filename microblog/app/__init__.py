53da5311de6be7ea15a65eaf28bb7c4b40e23ee7
Amber-Williams <amberswebdesign@gmail.com>
2021-09-04 19:35:45 +0100

    feat: error logging

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -14,6 +17,18 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 #The bottom import is a workaround to circular imports, a common problem with Flask applications.
-from app import routes, models 
+from app import routes, models, errors
