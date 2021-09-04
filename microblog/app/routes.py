ef85c2ce0fdc9dc49a46bc8cd0ba27702ddaf855
Amber-Williams <amberswebdesign@gmail.com>
2021-09-04 19:41:16 +0100

    fix: same username bug

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -97,7 +97,7 @@ def user(username):
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         username = form.username.data
         about_me = form.about_me.data
